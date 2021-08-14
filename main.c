cdfbd5de0fc951f4a6a1d8ca7074dc29f8672053
Leeqviz <leeqviz@gmail.com>
2021-08-14 16:36:05 +0300

    update

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
-// .exe = https://drive.google.com/file/d/1t07qsqBMATGKx0zoPQ27FVccCdMnzyOl/view?usp=sharing
+//
+// .exe = https://drive.google.com/file/d/1KqXQ5YgoBqOX3fFZMen17KCIX_vPrSY7/view?usp=sharing
+//
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,64 +8,73 @@
 #include <conio.h>
 
 void main (void) {
-	srand(time(NULL));
 
-	int array_length;
-	printf("Input array length (less than 100000): ");
-	while (1) {
-		if (scanf("%d", &array_length) == 1) {
-			if (array_length > 100000 || array_length <= 0) {
-				array_length = 100000;
-				printf("Array length is %d", array_length);
-			}
-			break;
-		}
-		else {
-			printf("Please, Input an integer value: ");
+	int exit = 1;
+	while (exit) {
+		exit = 0;
+		srand(time(NULL));
+
+		int array_length;
+		printf("Input array length (less than 100000): ");
+		while (1) {
 			fflush(stdin);
-		}
-	}
-
-	clock_t tic = clock();
-
-	int arr[array_length];
-	for (int i = 0; i < array_length; i++)
-		arr[i] = rand() % array_length;
-
-	printf("Generated array:\n");
-	for (int i = 0; i < array_length; i++)
-		printf("%d ", arr[i]);
-	
-	int left, right, tmp, counter = 0, flag = 1;
-	printf("\nSorting...");
-	while (flag) {
-		flag = 0;
-		left = rand() % array_length;
-		right = rand() % array_length;
-		//printf("\nNow indexes are %d and %d", left, right);
-
-		if ((left < right && arr[left] > arr[right]) || (left > right && arr[left] < arr[right])) {
-			tmp = arr[left];
-			arr[left] = arr[right];
-			arr[right] = tmp;
-		}
-		
-		for (int j = 0; j < array_length-1; j++) {
-			if (arr[j] > arr[j+1]) {
-				flag = 1;
-				//printf("\nNew iteration");
+			if (scanf("%d", &array_length) == 1) {
+				if (array_length > 100000 || array_length <= 0) {
+					array_length = 100000;
+					printf("Array length is %d", array_length);
+				}
 				break;
 			}
+			else printf("Input an integer value: ");
 		}
-		counter++;
-	}
-	printf("\nIterations: %d", counter);
 
-	printf("\nSorted array:\n");
-	for (int i = 0; i < array_length; i++)
-		printf("%d ", arr[i]);
-	
-	clock_t toc = clock();
-	printf("\nElapsed: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+		clock_t tic = clock();
+
+		int arr[array_length];
+		for (int i = 0; i < array_length; i++)
+			arr[i] = rand() % array_length;
+
+		printf("Generated array:\n");
+		for (int i = 0; i < array_length; i++)
+			printf("%d ", arr[i]);
+
+		int left, right, tmp, counter = 0, flag = 1;
+		printf("\nSorting...");
+		while (flag) {
+			flag = 0;
+			left = rand() % array_length;
+			right = rand() % array_length;
+
+			if ((left < right && arr[left] > arr[right]) || (left > right && arr[left] < arr[right])) {
+				tmp = arr[left];
+				arr[left] = arr[right];
+				arr[right] = tmp;
+			}
+
+			for (int j = 0; j < array_length-1; j++)
+				if (arr[j] > arr[j+1]) {
+					flag = 1;
+					break;
+				}
+
+			counter++;
+		}
+		printf("\nIterations: %d", counter);
+
+		printf("\nSorted array:\n");
+		for (int i = 0; i < array_length; i++)
+			printf("%d ", arr[i]);
+
+		clock_t toc = clock();
+		printf("\nElapsed: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+
+		fflush(stdin);
+		char choice;
+		printf("Do you want to continue? [Y/N]: ");
+		scanf("%c", &choice);
+		if (choice == 'y' || choice == 'Y') exit = 1;
+	}
+
+	printf("Press any key...");
 	getch();
 }
